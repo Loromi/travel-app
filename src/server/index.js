a1240b0c7166b05b3e1b845364b7526d49099f70
Romi <>
2021-07-27 20:01:16 +0200

    docs: renamed server.js to src/server/index.js

--- src/server/index.js
+++ src/server/index.js
@@ -2,40 +2,38 @@
 projectData = {};
 
 // Require Express to run server and routes
-const express = require('express');
+const express = require("express");
 
 // Start up an instance of app
 const app = express();
 
-
-
 /* Middleware*/
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-const cors = require('cors');
-const { response } = require('express');
+const cors = require("cors");
+const { response } = require("express");
 app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("website"));
 
 // Setup Server
 const port = 3000;
 
-const server = app.listen(port, ()=>{console.log(`running on localhost: ${port}`)});
-
+const server = app.listen(port, () => {
+  console.log(`running on localhost: ${port}`);
+});
 
 // server-side GET route
-app.get('/all', (req,res)=> {
-    res.send(projectData);
+app.get("/all", (req, res) => {
+  res.send(projectData);
 });
 
 // server-side POST route
-app.post('/add', (req,res)=> {
-    projectData = req.body;
+app.post("/add", (req, res) => {
+  projectData = req.body;
 });
