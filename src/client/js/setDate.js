9af33eac23297c9e919a174f3b7efdd263e5d800
Romi <loromi94@gmail.com>
2021-07-30 15:46:00 +0200

    fix: check for correct format

--- src/client/js/setDate.js
+++ src/client/js/setDate.js
@@ -1,10 +1,15 @@
 const setDate = () => {
-  // Create a new date instance dynamically with JS
-  let d = new Date();
-  let newDate = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
-  // Set value of the date input to the generated date
   const inputDate = document.querySelector("#startDate");
-  inputDate.value = newDate;
+  let d = new Date(); // Create a new date instance dynamically with JS
+  // check month to use correct format: YYYY-MM-DD
+  let month = d.getMonth() + 1;
+  if (month < 10) {
+    let newDate = `${d.getFullYear()}-0${d.getMonth() + 1}-${d.getDate()}`;
+    inputDate.value = newDate; // Set value of the date input to the generated date
+  } else {
+    let newDate = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
+    inputDate.value = newDate; // Set value of the date input to the generated date
+  }
 };
 
 export { setDate };
