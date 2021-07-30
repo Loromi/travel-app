581f32b59eee6cf47904b9d7f75601a089ae5131
Romi <loromi94@gmail.com>
2021-07-30 16:58:52 +0200

    chore: set min property of date input to current date to exclude dates in the past

--- src/client/js/setDate.js
+++ src/client/js/setDate.js
@@ -6,9 +6,13 @@ const setDate = () => {
   if (month < 10) {
     let newDate = `${d.getFullYear()}-0${d.getMonth() + 1}-${d.getDate()}`;
     inputDate.value = newDate; // Set value of the date input to the generated date
+    inputDate.min = newDate;
+    return newDate;
   } else {
     let newDate = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
     inputDate.value = newDate; // Set value of the date input to the generated date
+    inputDate.min = newDate;
+    return newDate;
   }
 };
 
