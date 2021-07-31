b836ee9fd965dcd8fc5368b9ab390c76629c3372
Romi <>
2021-07-31 15:05:56 +0200

    feat: store desired weather data

--- src/client/js/apiRequest.js
+++ src/client/js/apiRequest.js
@@ -25,12 +25,16 @@ const getWeatherbit = async () => {
   const apiKey = "5d7a729b05cb4984b1917ce1c20941bb";
   // let url = `https://api.weatherbit.io/v2.0/forecast/daily?&lat=${geoData.latitude}&lon=${geoData.longitude}&key=${apiKey}`;
   let url = `https://api.weatherbit.io/v2.0/forecast/daily?&lat=51.508&lon=0.125&key=${apiKey}`;
-  // let geoData = {};
+  let weatherData = {};
   const req = await fetch(url);
   try {
     const data = await req.json();
-    console.log(data);
-    return data;
+    weatherData.temp = data.data[0].temp;
+    weatherData.description = data.data[0].weather.description;
+    weatherData.msSunrise = data.data[0].sunrise_ts;
+    weatherData.msSunset = data.data[0].sunset_ts;
+    console.log(weatherData);
+    return weatherData;
   } catch (error) {
     console.log("error in getWeatherbit()", error);
   }
