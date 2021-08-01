9b3388519802d19eb13662ba3e2e9ee0c2c06c94
Romi <>
2021-08-01 14:40:22 +0200

    fix: specified value "2021-08-1" does not conform to the required format, "yyyy-MM-dd".

--- src/client/js/setDate.js
+++ src/client/js/setDate.js
@@ -3,7 +3,13 @@ const setDate = () => {
   let d = new Date(); // Create a new date instance dynamically with JS
   // check month to use correct format: YYYY-MM-DD
   let month = d.getMonth() + 1;
-  if (month < 10) {
+  let day = d.getDate();
+  if (month < 10 && day < 10) {
+    let newDate = `${d.getFullYear()}-0${d.getMonth() + 1}-0${d.getDate()}`;
+    inputDate.value = newDate; // Set value of the date input to the generated date
+    inputDate.min = newDate;
+    return newDate;
+  } else if (month < 10 && day >= 10) {
     let newDate = `${d.getFullYear()}-0${d.getMonth() + 1}-${d.getDate()}`;
     inputDate.value = newDate; // Set value of the date input to the generated date
     inputDate.min = newDate;
