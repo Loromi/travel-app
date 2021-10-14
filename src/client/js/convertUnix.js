afc47c090243f3a1999f1a865a5737de4d9f1f51
Romi <loromi94@gmail.com>
2021-10-14 16:42:53 +0200

    chore: display sunrise and sunset in correct time format

--- src/client/js/convertUnix.js
+++ src/client/js/convertUnix.js
@@ -1,6 +1,14 @@
 const convertToTime = (unixTimestamp) => {
   const dateObject = new Date(unixTimestamp * 1000);
-  const utc = dateObject.toUTCString();
+  // const utc = dateObject.toUTCString();
+  const utc = {};
+  utc.hours = dateObject.getHours();
+  let minutes = dateObject.getMinutes();
+  if (minutes < 10) {
+    utc.minutes = `0${minutes}`;
+  } else {
+    utc.minutes = minutes;
+  }
   console.log(`utc: ${utc}`);
   return utc;
 };
