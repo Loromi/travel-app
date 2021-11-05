303b5e0f4e9086a0284ac6bf2865c7539d4a109e
Romi <loromi94@gmail.com>
2021-11-05 11:01:00 +0100

    fix: import polaroid logo correctly to set as placeholder for input card

--- src/client/index.js
+++ src/client/index.js
@@ -3,12 +3,17 @@ import { setDate } from "./js/setDate.js";
 import "./styles/style.scss";
 import "./styles/polaroid.scss";
 import "./styles/input.scss";
+import polaroidLogo from "./media/polaroid.png";
 
 document.addEventListener("DOMContentLoaded", loadingComplete);
 
 function loadingComplete() {
   console.log("DOM loaded!");
   setDate();
+
+  const inputLogo = document.querySelector("#inputLogo");
+  inputLogo.innerHTML = `<img src="${polaroidLogo}" alt="cam-logo.png" width="300" height="300"/>`;
+
   const submitBtn = document.querySelector("#generate");
   submitBtn.addEventListener("click", handleSubmit);
 }
