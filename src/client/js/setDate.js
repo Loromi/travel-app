830878ec7eb953ab5a03c79070ac0826ddbda8ee
Romi <loromi94@gmail.com>
2021-11-26 13:54:28 +0100

    refactor: split up setdate functionality to improve readability

--- src/client/js/setDate.js
+++ src/client/js/setDate.js
@@ -1,33 +1,31 @@
+// check date to use correct format: YYYY-MM-DD
+const checkFormat = (year, month, day) => {
+  if (month < 10 && day < 10) {
+    let newDate = `${year}-0${month}-0${day}`;
+    return newDate;
+  } else if (month >= 10 && day < 10) {
+    let newDate = `${year}-${month}-0${day}`;
+    return newDate;
+  } else if (month < 10 && day >= 10) {
+    let newDate = `${year}-0${month}-${day}`;
+    return newDate;
+  } else {
+    let newDate = `${year}-${month}-${day}`;
+    return newDate;
+  }
+};
+
 const setDate = () => {
   const inputDate = document.querySelector("#startDate");
   let d = new Date(); // Create a new date instance dynamically with JS
 
-  // check month to use correct format: YYYY-MM-DD
   let year = d.getFullYear();
   let month = d.getMonth() + 1;
   let day = d.getDate();
 
-  if (month < 10 && day < 10) {
-    let newDate = `${year}-0${month}-0${day}`;
-    inputDate.value = newDate; // Set value of the date input to the generated date
-    inputDate.min = newDate;
-    return newDate;
-  } else if (month >= 10 && day < 10) {
-    let newDate = `${year}-${month}-0${day}`;
-    inputDate.value = newDate; // Set value of the date input to the generated date
-    inputDate.min = newDate;
-    return newDate;
-  } else if (month < 10 && day >= 10) {
-    let newDate = `${year}-0${month}-${day}`;
-    inputDate.value = newDate; // Set value of the date input to the generated date
-    inputDate.min = newDate;
-    return newDate;
-  } else {
-    let newDate = `${year}-${month}-${day}`;
-    inputDate.value = newDate; // Set value of the date input to the generated date
-    inputDate.min = newDate;
-    return newDate;
-  }
+  let newDate = checkFormat(year, month, day);
+  inputDate.value = newDate; // Set value of the date input to the generated date
+  inputDate.min = newDate;
 };
 
 export { setDate };
